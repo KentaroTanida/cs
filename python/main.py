d3571397e2de4f131f2f16c4ea3f835d566fba53
kentarotanida <main@kentaro-tanida.com>
2019-03-13 16:22:01 +0900

    [add] first commit

---
+++ python/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main():
+    print("Hello World")
+
+
+#print(__name__)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+#ref: http://blog.pyq.jp/entry/Python_kaiketsu_180207
+#ref: http://azuuun-memorandum.hatenablog.com/entry/2015/05/09/002549
