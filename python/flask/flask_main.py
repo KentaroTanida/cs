eac992c9358f1acef846130b95ba7b5313b2da36
kentarotanida <main@kentaro-tanida.com>
2019-03-16 22:06:33 +0900

    [add] first commit

---
+++ python/flask/flask_main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route('/v1/target', methods=['POST'])
+def new_machine():
+    #print(request.data.decode('utf-8'))
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    print(name['fuga'])
+
+    return 'THX'
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=5050)
+
+
+
+'''
+#test command
+curl -H "Content-type: application/json" -X POST -d '{"name": "fuga"}' 0.0.0.0:5050/v1/target
+'''
+
+
---
+++ python/flask/wp_curl.php
@@ -0,0 +1,30 @@
+//API
+//$url = 'http://0.0.0.0:5050';
+$url = 'http://0.0.0.0:5050/v1/target';
+
+$data = array(
+'name'=>$users,
+'phone'=>'090-xxxx-xxxx'
+);
+
+$data_json = json_encode($data);
+echo $data_json;
+
+
+// curlを初期化
+$ch = curl_init();
+
+// 設定!
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_URL, $url); // 送り先
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json); // 送信データ
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 実行結果取得の設定
+
+//echo $url;
+
+// 実行！
+$output = curl_exec($ch);
+echo $output;
+// リソースを閉じる
+curl_close($ch);
