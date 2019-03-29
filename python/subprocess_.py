877bb58f8828de5915ccab303a470d7b4d16ca6c
kentarotanida <main@kentaro-tanida.com>
2019-03-29 10:36:27 +0900

    [add] first commit

---
+++ python/subprocess_.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+def main():
+
+    cmd = 'ls -la'
+    res = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    res.wait()
+    print(res)
+
+def main2():
+
+    cmd = ['ls -la']
+    res = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    res.wait()
+    print(res.communicate()[0].decode("utf8"))
+
+
+
+
+if __name__ == "__main__":
+    main()
+    main2()
+
+'''
+call
+check_call
+check_output
+Popen
+'''
---
+++ python/try_exception.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main():
+    a = 1
+    b = 0
+
+    try:
+        print(a / b)
+    
+    except Exception as e:
+        print(e)
+
+
+
+if __name__ == "__main__":
+    main()
