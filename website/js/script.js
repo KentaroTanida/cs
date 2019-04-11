2e598d6e833eeab8a81e389149eb30f98bf087be
kentarotanida <main@kentaro-tanida.com>
2019-04-11 22:06:11 +0900

    [up] 勉強コード追加

--- website/js/script.js
+++ website/js/script.js
@@ -1,16 +1,24 @@
 $(function() {
 
   //h1要素を隠す
-  $('h1').hide();  
+  //$('h1').hide();  
 
   //
   $('img').fadeOut(1500);
 
   //
-  $('p').slideUp(1500);
+  //$('p').slideUp(1500);
 
   $('#hide-text').click(function() {
     $('#text').slideUp();
+  });
+
+  $('#language-wrapper').hover(
+    function() {
+      $('.language-text').fadeIn();
+    },
+    function() {
+      $('.language-text').fadeOut();
   });
 
 });
