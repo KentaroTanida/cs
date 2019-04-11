92caa87b555574892d35d1a50729ce2d5a8b0044
kentarotanida <main@kentaro-tanida.com>
2019-04-11 18:15:57 +0900

    [add] first commit

---
+++ website/js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  //h1要素を隠す
+  $('h1').hide();  
+
+  //
+  $('img').fadeOut(1500);
+
+  //
+  $('p').slideUp(1500);
+
+  $('#hide-text').click(function() {
+    $('#text').slideUp();
+  });
+
+});
+
